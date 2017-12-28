0e5fca338489b7076751984a14037b093c382015
banphaeo34 <34806725+banphaeo34@users.noreply.github.com>
2017-12-28 11:12:06 +0700

    Update chapter-01.php

--- chapter-01.php
+++ chapter-01.php
@@ -1,6 +1,6 @@
 <?php
 
-include ('line-bot-api/php/line-bot.php');
+include ('line-bot-api/line-bot.php');
 
 $channelSecret = '551ec4feee0.....43cff0';
 $access_token  = '2og9ogezC8k.......5ZUEQQdB04t89/1O/w1cDnyilFU=';
