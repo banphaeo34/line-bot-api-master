1a7d82780f771a7f155dccb8d89fe687415b435e
banphaeo34 <34806725+banphaeo34@users.noreply.github.com>
2017-12-28 11:20:39 +0700

    Update chapter-01.php

--- chapter-01.php
+++ chapter-01.php
@@ -1,6 +1,6 @@
 <?php
 
-include ('line-bot-api/line-bot.php');
+include ('line-bot.php');
 
 $channelSecret = '81e31b8c15d77bef991fd13caae85506';
 $access_token  = '7EmpPR9GthiSeWpv1q0dBDMlUi7DoYwdVTTkPcnod2IcMAwZ5qXnT6t0ZE+YJw3aqbZwl++iEfa2GkAR1yoQ4nZDxwNmNfMkzsnOTBZj7d2qsfYx2hOWU8BNgH8AqjjuGWAR6FDkoS2TL9ktLPWpPgdB04t89/1O/w1cDnyilFU=';
