78ee5f11b07cda4d8bceba5fb4fd1b2b0f389377
banphaeo34 <34806725+banphaeo34@users.noreply.github.com>
2017-12-28 20:32:20 +0700

    Add files via upload

---
+++ bot_settings.php
@@ -0,0 +1,7 @@
+<?php
+/// การตั้งค่าเกี่ยวกับ bot ใน LINE Messaging API
+define('LINE_MESSAGE_CHANNEL_ID','1553567244');
+define('LINE_MESSAGE_CHANNEL_SECRET','81e31b8c15d77bef991fd13caae85506
+');
+define('LINE_MESSAGE_ACCESS_TOKEN','7EmpPR9GthiSeWpv1q0dBDMlUi7DoYwdVTTkPcnod2IcMAwZ5qXnT6t0ZE+YJw3aqbZwl++iEfa2GkAR1yoQ4nZDxwNmNfMkzsnOTBZj7d2qsfYx2hOWU8BNgH8AqjjuGWAR6FDkoS2TL9ktLPWpPgdB04t89/1O/w1cDnyilFU=');
+?>
