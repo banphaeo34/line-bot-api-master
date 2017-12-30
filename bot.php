7f803e33f7ab3e89e8ae4de685fe78a46a924a8c
banphaeo34 <34806725+banphaeo34@users.noreply.github.com>
2017-12-30 11:28:14 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -11,7 +11,7 @@ require_once './vendor/autoload.php';
 require_once 'bot_settings.php';
  
 // ติดต่อฐานข้อมูล
-require_once("dbconnect.php");
+//require_once("dbconnect.php");
  
 ///////////// ÊèÇ¹¢Í§¡ÒÃàÃÕÂ¡ãªé§Ò¹ class ¼èÒ¹ namespace
 use LINE\LINEBot;
